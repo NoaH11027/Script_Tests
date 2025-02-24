d537dfbb220e4c78362c0c85f76feec9e4ec8e4b
NoaH11027 <mark.iv.photo@gmail.com>
2025-02-24 14:49:56 +0100

    Update ipServerSearch.sh
    it started right here

--- playGround/ipServerSearch.sh
+++ playGround/ipServerSearch.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
+#
+#      simple script to create a server ip address list
+#
+#      Mark Luethke
+#      24. Berurary 2025
+#
+#
+######
 
+#      VARIABLES
+## 
+
+net1
+
+name="notebook"
+net="123.45.67."
+for ip in $( seq 1 255 ); do {
+    sleep 1; # be kind to the server, unauthorized user
+    text="$( nslookup $net$ip 2>&1 | grep "$name" )";
+    if [ -z "$text" ]; then continue; fi;
+    echo "$text";
+}; done
